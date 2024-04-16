a1cf0b755d818d4ff491e7f9e1cd3972d21dd934
Andre Giron <andre@spiralai.co>
2024-04-16 10:52:07 -0700

    Remove existing chat widget and comment out runllm chat widget.

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -5,7 +5,6 @@ import DocItemMetadata from "@theme/DocItem/Metadata";
 import DocItemLayout from "@theme/DocItem/Layout";
 import FeedbackForm from "@site/src/components/FeedbackForm";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import ChatbotItem from "@site/src/components/ChatbotItem";
 import styles from "./styles.module.css";
 
 export default function DocItem(props) {
@@ -28,7 +27,6 @@ export default function DocItem(props) {
           <MDXComponent />
         </DocItemLayout>
       </HtmlClassNameProvider>
-      <ChatbotItem />
       {metadata.editUrl && (
         <div className="row">
           <FeedbackForm
